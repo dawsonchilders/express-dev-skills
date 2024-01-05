f673be5f1479318bec4e468234bc3f231d6ab698
dawsonchilders <dawsonchilders@gmail.com>
2024-01-04 17:09:10 -0800

    Lab part 2 finished

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,16 +3,52 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    edit,
+    update,
+    delete: deleteSkill
 };
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+}
+
+function update(req, res) {
+  Skill.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`);
+}
+
+function edit(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/edit', {
+    title: 'Edit Skill',
+    skill
+  });
+}
+
+function create(req, res) {
+  Skill.create(req.body);
+  res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+  res.render('skills/new', { title: 'Add New Skill' });
+}
 
 function show(req, res) {
     const skill = Skill.getOne(req.params.id);
-    res.render('skills/show', { skill });
+    res.render('skills/show', {
+      title: 'About My Skills',
+      skill
+    });
 }
 
 function index(req, res) {
     res.render('skills/index', {
+      title: 'My Skills', 
       skills: Skill.getAll()
     });
   }
--- routes/skills.js
+++ routes/skills.js
@@ -4,10 +4,20 @@ const skillsCtrl = require('../controllers/skills')
 
 
 /* GET users listing. */
+
+
+router.get('/new', skillsCtrl.new);
+
 router.get('/', skillsCtrl.index);
 
 router.get('/:id', skillsCtrl.show);
-
-
+//POST SKills
+router.post('/', skillsCtrl.create);
+// GET /:id/edit
+router.get('/:id/edit', skillsCtrl.edit);
+//PUT /skills/:id
+router.put('/:id', skillsCtrl.update);
+//Delete /skill/:id
+router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
